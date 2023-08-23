bb64f7bf8645c08bed445841643be7f3eccfcfd9
Josua Hasler <josua.hasler@hotmail.com>
2023-08-23 16:24:58 +0200

    yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(name: "Holy Carrot", address:" 2-4 Hans Cres, London SW1X 0LH, United Kingdom", category: "french")
+Restaurant.create(name: "Flat Iron", address:"77 Curtain Rd, London EC2A 3BS, United Kingdom", category: "french")
+Restaurant.create(name: "Om Nom Nom", address:"Oberländerstrasse, 81371 München, Germany", category: "french")
+Restaurant.create(name: "Factory Girl", address:"Auguststraße 29c, 10119 Berlin, Germany", category: "french")
+Restaurant.create(name: "Nomad", address:"Borsigstraße 28, 10115 Berlin", category: "french")
